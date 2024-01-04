a9eee66cd82cad5499d5925b71d32fdc6ea38977
Pablo José da SIlva <pablojosecg@gmail.com>
2024-01-03 21:50:22 -0300

    Input para digitar a nota

---
+++ src/Media.java
@@ -0,0 +1,17 @@
+/*CRIAR QUATRO NOTAS DO TIPO DOUBLE
+ * E FAZER A MEDIA DESSAS QUATRO NOTAS
+ * NO FINAL IMPRIMIR NA TELA A MEDIA
+ * PRIMEIRO*/
+
+
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+
+
+    }
+}
+
