6daccddd72e9cf4574d5969bfab7f246751bd5e3
Pablo José da SIlva <pablojosecg@gmail.com>
2024-01-03 22:04:36 -0300

    adicionando notas

--- src/Media.java
+++ src/Media.java
@@ -9,7 +9,13 @@ import java.util.Scanner;
 public class Media {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
+        double nota1 = input.nextDouble();
+        double nota2 = input.nextDouble();
+        double nota3 = input.nextDouble();
+        double nota4 = input.nextDouble();
+
+
 
 
     }
