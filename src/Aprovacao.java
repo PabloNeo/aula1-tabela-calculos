0fb6e289a8816a208a85419978417c6a35c1a935
Pablo José da SIlva <pablojosecg@gmail.com>
2024-01-05 19:53:52 -0300

    Adicionando aprovação

---
+++ src/Aprovacao.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Aprovacao {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Qual foi sua nota 1?");
+        double nota1 = input.nextDouble();
+
+        System.out.println("Qual foi a sua nota 2");
+        double nota2 = input.nextDouble();
+
+        System.out.println("Qual foi a sua nota 3");
+        double nota3 = input.nextDouble();
+
+        System.out.println("Qual foi a sua nota 4");
+        double nota4 = input.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4.0;
+
+        System.out.println("A sua média é de: "+ media);
+
+        if (media >= 7.0){
+             System.out.println("Aprovado");
+        } else {
+             System.out.println("Não Aprovado");
+
+        }
+
+
+    }
+}
